d32f51a4e8c3233a0393e8ee04d0874e60511666
William Hatcher <william@hatcher.work>
2024-04-02 10:28:32 -0500

    removed txt and pdf from allowed extensions

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ from prometheus_flask_exporter import PrometheusMetrics
 
 # Settings
 UPLOAD_FOLDER = '/uploads'
-ALLOWED_EXTENSIONS = {'txt', 'pdf', 'png', 'jpg', 'jpeg', 'gif'}
+ALLOWED_EXTENSIONS = {'png', 'jpg', 'jpeg', 'gif'}
 
 app = Flask(__name__)
 app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
